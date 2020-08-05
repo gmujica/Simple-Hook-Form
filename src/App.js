fdb7f6cfc98795c1556126c1e3f47ceb8f3cd4d2
gmujica <gregorymujica1993@gmail.com>
2020-08-05 16:45:21 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useForm } from 'react-hook-form'
 
-function App() {
+export default function App() {
+  const { register, handleSubmit, errors   } = useForm()
+
+  const onSubmit = (data) => {
+    console.log(data)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" placeholder="Email" name="email" ref={register} />
+      <input 
+        type="password" 
+        placeholder="Password" 
+        name="password" 
+        ref={register({ required: 'PASSWOED REQUIRED', minLength: { value: 8, message: 'TOO SHORT PASSWORD'} })}
+      />
+      { errors.password && <p>{errors.password.message}</p> }
+      <input type="submit" />
+    </form>
   );
 }
 
-export default App;
+
