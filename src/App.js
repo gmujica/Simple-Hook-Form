f9071ebd169f21e6b5faa73bd5c9cdeb74199a3a
gmujica <gregorymujica1993@gmail.com>
2020-08-05 19:07:33 -0500

    material-ui added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from 'react'
-import { useForm } from 'react-hook-form'
+import SignIn from './components/signIn/SignIn'
 
 export default function App() {
-  const { register, handleSubmit, errors   } = useForm()
-
-  const onSubmit = (data) => {
-    console.log(data)
-  };
-
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input type="text" placeholder="Email" name="email" ref={register} />
-      <input 
-        type="password" 
-        placeholder="Password" 
-        name="password" 
-        ref={register({ required: 'PASSWOED REQUIRED', minLength: { value: 8, message: 'TOO SHORT PASSWORD'} })}
-      />
-      { errors.password && <p>{errors.password.message}</p> }
-      <input type="submit" />
-    </form>
-  );
+    <div>
+      <SignIn />
+    </div>
+  )
 }
 
 
